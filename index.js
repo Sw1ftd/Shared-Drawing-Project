7ad3cab73ff23154a5498e68cb42dc4279426412
Sw1ftd <43348289+Sw1ftd@users.noreply.github.com>
2019-10-30 14:14:50 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express()
+
+var server = app.listen(80)
+app.use(express.static('public'))
+
+var socket = require('socket.io')
+var io = socket(server)
+console.log("Server running")
+
+io.sockets.on('connection', newConnection)
+
+function newConnection(socket){
+    console.log('new connection: ' + socket.id)
+    
+    socket.on('mouse', mouseMsg)
+    
+    function mouseMsg(data) {
+        socket.broadcast.emiy('mouse', data)
+        console.log(data)
+    }
+}
